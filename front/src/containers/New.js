b9983ff4326c7a7154df20d46e2d3275b8c21f44
tsmrkk <koki827tt@gmail.com>
2019-10-19 03:32:24 +0900

    commit for rebasing master

--- front/src/containers/New.js
+++ front/src/containers/New.js
@@ -5,18 +5,19 @@ import { withStyles } from '@material-ui/core';
 
 @withStyles(theme => ({
   container: {
-    background: 'red',
+    background: 'linear-gradient(221.89deg, #C3C3C3 0%, rgba(162, 162, 162, 0.3) 100%)',
+    width: '100%',
+    height: '100%',
+    paddingTop: 44,
+    paddingBottom: 180,
+    fontFamily: 'Roboto',
+    color: '#FFFFFF',
   },
 }))
 class New extends React.Component {
   // constructor(props) {
   //   super(props);
   // }
-
-  onClick = () => {
-    this.props.history.push("/");
-    this.setState({ redirect: true });
-  };
 
   render() {
     const { classes } = this.props;
