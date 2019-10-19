b4f7f8b0479022d0ac4dfbcb4f931ffc55967c7f
Yuya Inagaki <yuya1756@live.jp>
2019-10-19 20:35:21 +0900

    新規名刺作成ページ

--- front/src/containers/New.js
+++ front/src/containers/New.js
@@ -3,6 +3,10 @@ import { withStyles } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+
+import "./css/header.css";
+import "./css/card.css";
+import "./css/create_tab.css";
 // TODO selectをmaterial uiからimportするなりして、値を入力できるようにする
 
 // import { Redirect } from "react-router-dom";
@@ -14,13 +18,8 @@ import Tab from '@material-ui/core/Tab';
     background: 'linear-gradient(221.89deg, #C3C3C3 0%, rgba(162, 162, 162, 0.3) 100%)',
     width: '100%',
     height: '100%',
-    paddingTop: 44,
-    paddingBottom: 180,
     fontFamily: 'Roboto',
-    color: '#FFFFFF',
-  },
-  cardWrapper : {
-    padding: '100px 5px',
+    color: '#E5E5E5',
   }
 }))
 class New extends React.Component {
@@ -47,13 +46,13 @@ class New extends React.Component {
     // const { redirect } = this.state;
     return (
       <div className={classes.container}>
-        <div className={classes.header}>
-          <p>戻る</p>
-          <p>meish</p>
+        <div className="header" type="new">
+          <p className="back"><span className="arrow"></span>戻る</p>
+          <p className="logo">meish</p>
         </div>
-        <div className={classes.subHeader}>
-          <p>シンプル1</p>
-          <button>完了</button>
+        <div className="subHeader">
+          <p className="themeTitle">シンプル1</p>
+          <span className="completeBtn">完了</span>
         </div>
         <div className={classes.cardWrapper}>
           <Card />
@@ -80,21 +79,20 @@ const Card = withStyles((theme) => ({
     fontFamily: 'Roboto',
     fontWeight: 'bold',
   },
-  title: {
-    margin: '0 auto',
-    paddingTop: 50,
-    height: 46,
-    fontSize: 35,
-  },
-  phrase: {
-    fontSize: 12,
-  },
+
 }))((props) => {
   const {classes} = props
   return (
-    <div>
-      <p className={classes.title}>meish</p>
-      <p className={classes.phrase}>早く簡単に名刺をもらえる</p>
+    <div className="meishi-outer">
+      <div className="meish-body" theme="2">
+        <img src={`${process.env.PUBLIC_URL}/themes/background/2_background.jpg`} />
+        <span className="kana">やまだ たろう</span>
+        <span className="name">山田太郎</span>
+        <span className="role">株式会社ハック　サーバーエンジニア</span>
+        <span className="info" info="0"><span type="address"></span> 100-1000　東京都 港区 12-3</span>
+        <span className="info" info="1"><span type="phone"></span> 090-0090-0090</span>
+        <span className="info" info="2"><span type="email"></span> yamada@hack.com</span>
+      </div>
     </div>
   )
 })
@@ -124,27 +122,87 @@ const TabHeader = withStyles((theme) => ({
 })
 
 const TabForm1= withStyles((theme) => ({
-  container: {
-    background: '#585858',
-  },
+  
 }))((props) => {
   const {classes} = props
   return (
-    <div className={classes.container}>
-      hello
+    <div className="tab_info">
+      <span class="message">入力したプロフィールは名刺にすぐに反映されるよ</span>
+      <form>
+        <span class="label complete">名前</span>
+        <input type="text" value="山田太郎" />
+        <span class="label complete">名前の読み方</span>
+        <input type="text" value="やまだたろう" />
+        <span class="label">名前下小文字</span>
+        <span class="explain">役職・所属など(例：株式会社トマト 事務)</span>
+        <input type="text" value="株式会社ハック サーバーエンジニア" />
+        <span class="label">1|通常文</span>
+        <span class="explain">郵便番号・住所・電話番号など</span>
+        <div class="info-menu">
+          <select class="type-select">
+            <option value="email">email</option>
+            <option value="phone">Tel</option>
+            <option value="address">〒</option>
+          </select>
+          <input class="content" type="text" value="株式会社ハック サーバーエンジニア" />
+        </div>
+        <span class="label">2|通常文</span>
+        <span class="explain">郵便番号・住所・電話番号など</span>
+        <div class="info-menu">
+          <select class="type-select">
+            <option value="email">email</option>
+            <option value="phone">Tel</option>
+            <option value="address">〒</option>
+          </select>
+          <input class="content" type="text" value="株式会社ハック サーバーエンジニア" />
+        </div>
+        <span class="label">3|通常文</span>
+        <span class="explain">郵便番号・住所・電話番号など</span>
+        <div class="info-menu">
+          <select class="type-select">
+            <option value="email">email</option>
+            <option value="phone">Tel</option>
+            <option value="address">〒</option>
+          </select>
+          <input class="content" type="text" value="株式会社ハック サーバーエンジニア" />
+        </div>
+      </form>
     </div>
   )
 })
 
 const TabForm2= withStyles((theme) => ({
-  container: {
-    background: '#585858',
-  },
+
 }))((props) => {
   const {classes} = props
   return (
-    <div className={classes.container}>
-      bye
+    <div className="tab_theme">
+      <div className="theme_list">
+        <div className="theme select">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/1_sample.jpg`} />
+          <span>テーマ１</span>
+        </div>
+        <div className="theme">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/2_sample.jpg`} />
+          <span>テーマ2</span>
+        </div>
+        <div className="theme">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/3_sample.jpg`} />
+          <span>テーマ3</span>
+        </div>
+        <div className="theme">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/4_sample.jpg`} />
+          <span>テーマ4</span>
+        </div>
+        <div className="theme">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/5_sample.jpg`} />
+          <span>テーマ5</span>
+        </div>
+        <div className="theme">
+          <img src={`${process.env.PUBLIC_URL}/themes/sample/6_sample.jpg`} />
+          <span>テーマ6</span>
+        </div>
+      </div>
     </div>
   )
 })
