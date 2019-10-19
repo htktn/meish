8be5504f4679122d62d734e0623dd8c245df6014
Yuya Inagaki <yuya1756@live.jp>
2019-10-19 15:05:10 +0900

    緑色ボタンコンポーネントの作成

---
+++ front/src/components/GreenBtn.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { withStyles } from '@material-ui/core';
+
+const GreenBtn = withStyles((theme) => ({
+  btn: {
+    background: '#0FA369',
+    borderRadius: '6px',
+    padding: '20px 0',
+    margin: '10px 32px',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: '14px',
+    lineHeight: '16px',
+    letterSpacing: '0.03em'
+  }
+}))((props) => {
+  const {classes} = props;
+    
+  return (
+    <div className={classes.btn}>
+      { props.title }
+    </div>
+  )
+})
+
+export default GreenBtn
