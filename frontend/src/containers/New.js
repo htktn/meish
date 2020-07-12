842cee720ee645bd36dcd5440029b719e3156acf
Koki Tsumura <koki827tt@gmail.com>
2020-07-12 17:40:22 +0900

    added validation for new card

--- frontend/src/containers/New.js
+++ frontend/src/containers/New.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { withStyles } from '@material-ui/core';
-import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Card from '../components/Card';
-import { getThemes, createCard } from '../modules/cards';
-import { Redirect } from "react-router-dom";
+import React from "react"
+import { withStyles } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Card from '../components/Card'
+import { getThemes, createCard } from '../modules/cards'
+import { Redirect } from "react-router-dom"
 
-import "./css/header.css";
-import "./css/create_tab.css";
+import "./css/header.css"
+import "./css/create_tab.css"
 
 
 //TODO 後でwithStylesが必要ないものは外す
@@ -23,7 +23,7 @@ import "./css/create_tab.css";
 }))
 class New extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       tabIndex: 0,
       selectedThemeId: 1,
@@ -55,38 +55,118 @@ class New extends React.Component {
     }).catch(err => console.log(err))
   }
 
-  onChange = (e, stateName) => {
-    const text = e.target.value.trim();
-    this.setState({ [stateName]: text });
-  };
+  converZenkakuToHankaku = a => {
+    // 10進数の場合
+    a = a.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+      return String.fromCharCode(
+        s.charCodeAt(0) - 65248
+      );
+    });
 
-  //TODO refactor and move this to module so that update method might be able to use this method
+    // 16進数の場合
+    a = a.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (s) => {
+      return String.fromCharCode(
+        s.charCodeAt(0) - 0xFEE0
+      );
+    });
+
+    return a;
+  }
+
+  onChange = (e, stateName) => {
+    const text = e.target.value.trim()
+    this.setState({ [stateName]: text })
+  }
+
+  isValidPhoneNumber = pn => {
+    const phoneNum = this.converZenkakuToHankaku(pn);
+    return /^(0[5-9]0\d{8}|0[1-9][1-9]\d{7})$/.test(phoneNum.replace(/-/g, ''))
+  }
+
+  isValidEmailAddress = value => {
+    const m = this.converZenkakuToHankaku(value);
+    console.log('m', m)
+    return m.includes("@");
+  }
+
+  // only checks whether name is empty or not
+  isValidName = n => {
+    return n !== ''
+  }
+
+  // only checks whether reading(kana) is empty or not
+  isValidReading = k => {
+    return k !==  ''
+  }
+
+  isValid = (type, value) => {
+    switch(type) {
+      case 'email':
+        return this.isValidEmailAddress(value)
+      case 'phone':
+        return this.isValidPhoneNumber(value)
+      case 'name':
+        return this.isValidName(value)
+      case 'kana':
+        return this.isValidReading(value)
+      default:
+        return null
+    }
+  }
+
+  // TODO refactor and move this to module so that update method might be able to use this method as well
+  // TODO 名前考え直す
+  // TODO typeIdをマジックナンバーにしないで、ちゃんと定数にする
   prepareSubmitData = () => {
-    const { name, kana, role, informations, selectedThemeId } = this.state;
+    const { name, kana, role, informations, selectedThemeId } = this.state
+    const errors = {}
+    if (!this.isValid('name', name)) {
+      errors.name = "名前は必須です"
+    }
+    if (!this.isValid('kana', kana)) {
+      errors.kana = "読み方は必須です"
+    }
     let infos = []
+    errors.info = {}
     informations.map(info => {
       let typeId = 1
-      if (info.type === 'phone') typeId = 2
-      if (info.type === 'address') typeId = 3
+      if (info.type === 'email') {
+        typeId = 1
+        if (!this.isValid('email', info.content)) {
+          errors.info.email = "正しいメールアドレスを入力してください"
+        }
+      } else if (info.type === 'phone') {
+        typeId = 2
+        if (!this.isValid('phone', info.content)) {
+          errors.info.phone = "正しい電話番号を入力してください"
+        }
+      } else if (info.type === 'address') {
+        typeId = 3
+      }
       infos.push({
         "content": `${info.content}`,
         "type_id": typeId,
       })
       return info
     })
-    return {
+    const res = {
       "informations": infos,
       "role": role,
       "name": name,
       "kana": kana,
       "theme_id": selectedThemeId,
     }
+
+    return {res, errors}
   }
 
   onSubmit = () => {
-    const body = this.prepareSubmitData()
-    createCard(body).then(result => {
-      this.props.history.push("/cards/new");
+    const {res, errors} = this.prepareSubmitData()
+    if (Object.values(errors).length === 0) {
+      return
+    }
+    createCard(res).then(result => {
+      this.props.history.push("/cards/new")
       this.setState({redirect: true, createdCardId: result.id })
     }).catch(err => console.log(err))
   }
@@ -108,20 +188,20 @@ class New extends React.Component {
   }
 
   onPullTextChange = (e, index) => {
-    const text = e.target.value.trim();
-    let { informations } = this.state;
+    const text = e.target.value.trim()
+    let { informations } = this.state
     informations[index].content = text
     this.setState({ informations })
   }
 
   onClick = () => {
-    this.props.history.push("/cards");
-    this.setState({ redirect: true });
+    this.props.history.push("/cards")
+    this.setState({ redirect: true })
   }
 
   render() {
-    const { classes } = this.props;
-    let { tabIndex, name, kana, role, informations, themes, selectedThemeId, selectedThemeName,redirect } = this.state;
+    const { classes } = this.props
+    let { tabIndex, name, kana, role, informations, themes, selectedThemeId, selectedThemeName,redirect } = this.state
     const card = {
       name: name,
       kana: kana,
@@ -149,7 +229,7 @@ class New extends React.Component {
           <LayoutSelect themes={themes} selectedThemeId={selectedThemeId} onClick={this.onSelectTheme}/>
         }
       </div>
-    );
+    )
   }
 }
 
@@ -178,7 +258,7 @@ const TabHeader = withStyles((theme) => ({
 //TODO placeholderをつけたい
 const ProfileForm = withStyles((theme) => ({
 }))((props) => {
-  const { onPullTextChange, onToggle, onChange, card, onSubmit } = props;
+  const { onPullTextChange, onToggle, onChange, card, onSubmit } = props
   return (
     <div className="tab_info">
       <span className="message">入力したプロフィールは名刺にすぐに反映されるよ</span>
