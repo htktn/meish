7cdcad16b12f1db4646be8dc65b9bf11a471848c
Koki Tsumura <koki827tt@gmail.com>
2020-08-03 23:08:36 +0900

    added server.js

--- frontend/webpack.config.local.js
+++ frontend/webpack.config.local.js
@@ -4,9 +4,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: __dirname + '/src/index.js',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  // entry: {
+  //   server: path.resolve(__dirname, 'src/server.js'),
+  // },
+  target: 'node',
   output: {
-    path: __dirname + "/dist",
+    path: path.resolve(__dirname, "dist"),
     filename: 'bundle.js',
     publicPath: '/',
   },
@@ -14,7 +18,7 @@ module.exports = {
     historyApiFallback: true,
     host: '0.0.0.0',
     port: 8080,
-    watchContentBase: true,
+    contentBase: path.join(__dirname, 'dist'),
     disableHostCheck: true
   },
   module: {
@@ -23,26 +27,41 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            plugins: [
+              ["@babel/plugin-proposal-decorators", {"legacy": true}],
+              "@babel/plugin-proposal-class-properties",
+            ]
+          }
+        },
       },
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       },
       {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
         test: /\.(png|jpe?g)$/i,
         loader: 'url-loader',
-        options: {
-          limit: 8192,
-        },
+        // options: {
+        //   limit: 8192,
+        // },
       },
     ],
   },
   plugins: [
     new Dotenv({path: './.env.local'}),
     new HtmlWebpackPlugin({
-      template: 'public/index.html'
+      template: 'public/index.html',
+      excludeChunks: ['server']
     })
   ]
 }
